750453ddeaf289296b1682d62013df453b9f76ca
System0010 <emilio.arevalozuniga@gmail.com>
2023-03-28 12:08:59 +0200

    app1+functions01 class ==> ejercicio IVA

--- src/app1.java
+++ src/app1.java
@@ -1,12 +1,10 @@
-public class app1{
-  public static void main(String[] args){
+public class app1 {
+  public static void main(String[] args) {
     Functions01 f = new Functions01();
     double dFinalPrice = f.getPriceWiva(20.3d);
-    
     System.out.println("the final price is " + dFinalPrice + "$");
 
     int iFinalPrice = f.getPriceWiva(300);
-
     System.out.println("the Integer final price is " + iFinalPrice + "$");
   }
 }
