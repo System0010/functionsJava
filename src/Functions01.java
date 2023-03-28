83a9fcd8780e5c78a9ec2fd3ba28f2214ef69409
System0010 <emilio.arevalozuniga@gmail.com>
2023-03-28 11:57:33 +0200

    app1+functions01 class ==> ejercicio IVA

---
+++ src/Functions01.java
@@ -0,0 +1,9 @@
+public class Functions01 {
+  public double getPriceWiva(double dSetPrice){
+    return dSetPrice += (dSetPrice*0.21);
+  }
+
+  public int getPriceWiva(int iSetPrice){
+    return iSetPrice += (iSetPrice*0.21);
+  }
+}
